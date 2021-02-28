7d01f312307104322509e31b54001e665a0706b7
Your Name <nguyenxngocminhkhoi@gmail.com>
2021-02-28 21:48:07 +0700

    commit at 21:47 28/02/2021

--- crawler/action/action.php
+++ crawler/action/action.php
@@ -1,7 +1,22 @@
 <?php
+require_once dirname(__FILE__, 2) . "/libs/simple_html_dom.php";
+require_once dirname(__FILE__, 2) . "/model/dao/phananh_chuaxuly_dao.php";
+require_once dirname(__FILE__, 2) . "/model/dto/phananh_chuaxuly.php";
 
 class action
 {
   private $url = "https://tuongtac.thuathienhue.gov.vn/?act=dxl";
-  private $pre_param = 'ctl05$btnLink_';
+
+  /** 
+   * Scrape the webpage "tuongtac.thuathienhue.gov.vn/?act=dxl" and find out the id of
+   * latest "phananh" object which was uploaded on website
+   */
+  public function find_max_index()
+  {
+    $html_page_dxl = file_get_html($this->url);
+    $anchor_elements = $html_page_dxl->find(".PhanAnhTB_Nen .PhanAnhTB_Dong .PhanAnhTB_TieuDe a");
+    $href_of_anchor_element = $anchor_elements[0]->href;
+    $id_of_anchor_element = (int) str_replace("?pa=", "", $href_of_anchor_element);
+    return $id_of_anchor_element;
+  }
 }
--- crawler/model/dao/phananh_chuaxuly_dao.php
+++ crawler/model/dao/phananh_chuaxuly_dao.php
@@ -5,7 +5,8 @@ require_once dirname(__FILE__, 3) . "/db_connector/db.php";
 class phananh_chuaxuly_dao
 {
   private $phananh, $conn;
-  private $SQL_INSERT = "Insert into phananh_chua_xuly values (?,?,?,?,?,?,?)",
+  private
+    $SQL_INSERT = "Insert into phananh_chua_xuly values (?,?,?,?,?,?)",
     $SQL_SELECT = "Select * from phananh_chua_xuly ",
     $SQL_UPDATE_DA_XEM = "Update phananh_chua_xuly SET da_xem = '?' WHERE id = '?' ",
     $SQL_DELETE = "Delete from phananh_chua_xuly where id = '?'";
@@ -25,11 +26,82 @@ class phananh_chuaxuly_dao
    * Else, return true
    * @param phananh_chuaxuly $phananh
    */
-  private function checkif_phananh_is_new(int $id)
+  public function checkif_phananh_is_new(int $id)
   {
     // $id = $phananh->id;
     $SQL_SELECT = "Select * from phananh_chua_xuly where id = " . $id;
     $result = $this->conn->query($SQL_SELECT);
     return ($result->num_rows == 0);
+  }
+
+  /** 
+   * Create a "phananh" object
+   */
+  public function create(phananh_chuaxuly $phananh)
+  {
+    $SQL_INSERT = "Insert into phananh_chua_xuly values (?,?,?,?,?,?,?)";
+    $stmt = $this->conn->prepare($SQL_INSERT);
+    $stmt->bind_param(
+      "issssi",
+      $phananh->id,
+      $phananh->noi_dung,
+      $phananh->ngay_update,
+      $phananh->donvi_xuly,
+      $phananh->thoi_han,
+      ($phananh->is_new) ? 0 : 1
+    );
+    $stmt->execute();
+  }
+
+  /** Read all the "phananh" object saved in database */
+  public function read_all()
+  {
+    $SQL_SELECT = "Select * from phananh_chua_xuly ";
+    $result = $this->conn->query($SQL_SELECT);
+    $array = [];
+    while ($row = $result->fetch_assoc()) {
+      array_push($array, $row);
+    }
+    return $array;
+  }
+
+  /** 
+   * Read all the "phananh" object saved in database 
+   * which have the {$value} on the column {$name_of_col} in database table
+   */
+  public function read_by_column(string $name_of_col, $value)
+  {
+    $SQL_SELECT = "Select * from phananh_chua_xuly where {$name_of_col} = " . $value;
+    $result = $this->conn->query($SQL_SELECT);
+    $array = [];
+    while ($row = $result->fetch_assoc()) {
+      array_push($array, $row);
+    }
+    return $array;
+  }
+
+  /** 
+   * Change state of "phananh" object from "$da_xem=0" to '$da_xem = 1';
+   * or we can change state of "phananh" become "$da_xem=0", by passing "0" value 
+   * for the second parameter ($da_xem)
+   */
+  public function update_set_da_xem(int $id, int $da_xem = 1)
+  {
+    $SQL_UPDATE_DA_XEM = "Update phananh_chua_xuly SET da_xem = '?' WHERE id = '?' ";
+    $stmt = $this->conn->prepare($SQL_UPDATE_DA_XEM);
+    $stmt->bind_param("ii", $da_xem, $id);
+    $stmt->execute();
+  }
+
+
+  /** 
+   * Delete the specified-id "phananh" object
+   */
+  public function delete(int $id)
+  {
+    $SQL_DELETE = "Delete from phananh_chua_xuly where id = '?'";
+    $stmt = $this->conn->prepare($SQL_DELETE);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
   }
 }
